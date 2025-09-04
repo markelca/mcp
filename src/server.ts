17bd9a701dba399c1d5a17ee0701eb8b1dc90053
Markel Cuesta <markel.cuesta@roadsurfer.com>
2025-09-04 19:16:48 +0200

    Resource template

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
 import fs from "node:fs/promises";
@@ -71,6 +74,46 @@ server.resource(
         {
           uri: uri.href,
           text: JSON.stringify(users),
+          mimeType: "application/json",
+        },
+      ],
+    };
+  },
+);
+
+server.resource(
+  "user-details",
+  new ResourceTemplate("users://{userId}/profile", {
+    list: undefined,
+  }),
+  {
+    description: "Get a user's details from the database",
+    title: "Users",
+    mimeType: "application/json",
+  },
+  async (uri, { userId }) => {
+    const users = await import("./data/users.json", {
+      with: { type: "json" },
+    }).then((m) => m.default);
+    const user = users.find((u) => u.id === parseInt(userId as string));
+
+    if (user === null) {
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            text: JSON.stringify({ error: `User ${userId} not found` }),
+            mimeType: "application/json",
+          },
+        ],
+      };
+    }
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(user),
           mimeType: "application/json",
         },
       ],
