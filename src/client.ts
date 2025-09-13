f56068400b4b970c4da88ae4ac5cb2d3a96b2e15
Markel Cuesta <markel.cuesta@roadsurfer.com>
2025-09-13 14:43:18 +0200

    Client resources

--- src/client.ts
+++ src/client.ts
@@ -58,9 +58,32 @@ async function main() {
         } else {
           await handleTool(tool);
         }
-        // console.log(toolName);
         break;
       case "Resources":
+        const resourceUri = await select({
+          message: "Select a resource",
+          choices: [
+            ...resources.map((resource) => ({
+              name: resource.name,
+              value: resource.uri,
+              description: resource.description,
+            })),
+            ...resourceTemplates.map((template) => ({
+              name: template.name,
+              value: template.uriTemplate,
+              description: template.description,
+            })),
+          ],
+        });
+        const uri =
+          resources.find((r) => r.uri === resourceUri)?.uri ??
+          resourceTemplates.find((rt) => rt.uriTemplate === resourceUri)
+            ?.uriTemplate;
+        if (uri == null) {
+          console.error("Resource not found");
+        } else {
+          await handleResource(uri);
+        }
         break;
       case "Prompts":
         break;
@@ -80,6 +103,27 @@ async function handleTool(tool: Tool) {
   const res = await mcp.callTool({ name: tool.name, arguments: args });
 
   console.log((res.content as [{ text: string }])[0].text);
+}
+
+async function handleResource(uri: string) {
+  let finalUri = uri;
+  const paramMatches = uri.match(/{([^}]+)}/g);
+
+  if (paramMatches != null) {
+    for (const paramMatch of paramMatches) {
+      const paramName = paramMatch.replace("{", "").replace("}", "");
+      const paramValue = await input({
+        message: `Enter value for ${paramName}:`,
+      });
+      finalUri = finalUri.replace(paramMatch, paramValue);
+    }
+  }
+  const res = await mcp.readResource({
+    uri: finalUri,
+  });
+  console.log(
+    JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2),
+  );
 }
 
 main();
